84d0dc226669827b1ee4d93dad9a2bd25a484cf9
Asamiya <Asamiya_Shiina@hotmail.com>
2026-09-09 20:31:39 +0800

    新增 Java 测试程序

---
+++ jvav/test/ts.java
@@ -0,0 +1,16 @@
+package jvav.test;
+
+public class ts {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Java 测试成功！");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+
+        String name = "Java";
+        System.out.println("欢迎使用 " + name + " 编程语言！");
+    }
+}
