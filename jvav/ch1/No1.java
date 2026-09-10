804497913c79281c1195e47ee128422dec78b9fb
Asamiya <Asamiya_Shiina@hotmail.com>
2026-09-10 10:29:08 +0800

    小小修改

--- jvav/ch1/No1.java
+++ jvav/ch1/No1.java
@@ -1,6 +1,7 @@
 package jvav.ch1;
 public class No1 {
     public static void main(String[] args) {
+        String name = "Java";
         System.out.println("Hello, World!");
         System.out.println("欢迎使用 " + name + " 编程语言！");
     }
